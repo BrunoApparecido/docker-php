aef90d9992e83f2fea8b9ba511520fd758233e7d
Bruno Alexandre <bruno.apparecido@hotmail.com>
2020-05-19 22:59:47 -0300

    Add mysql

--- www/test.php
+++ www/test.php
@@ -1,2 +1,16 @@
 <?php
-phpinfo();
+
+
+$servername = "db";
+$username = "root";
+$password = "mydb123";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Mysql Connected successfully";
+?>
